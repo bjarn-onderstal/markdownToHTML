d3e3d6ff6ea5e81675d5bb7b6835793d10d2aab0
Hunter Koppen <hunter.koppen@kobeon.nl>
2024-02-21 16:59:45 +0100

    remove more unused stuff

--- src/components/MarkDownView.jsx
+++ src/components/MarkDownView.jsx
@@ -1,29 +1,9 @@
 import { Component, createElement } from "react";
 
 import MarkdownView from "react-showdown";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 
 export class MarkDownText extends Component {
-    state = {
-        open: false,
-        editedvalue: null
-    };
-
-    componentDidUpdate(prevProps) {
-        // Check if widget has loaded the url data
-        console.log(this.props.openBoolean);
-        if (this.props.openBoolean && this.props.openBoolean.status === "available") {
-            if (prevProps.openBoolean !== this.props.openBoolean && this.props.openBoolean !== this.state.editedvalue) {
-                this.setState({ open: this.props.openBoolean.value });
-            }
-        }
-    }
-
-    onDismiss = () => {
-        this.setState({ open: false, editedvalue: false });
-        this.props.openBoolean.setValue(false);
-    };
-
     sanitizeHtml = html => {
         if (this.props.sanitize) {
             return DOMPurify.sanitize(html);
